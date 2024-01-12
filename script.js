6da4b6158bd592d02b34a6caf7c5eab5d6e2e628
Axel Westman <111924075+AxelWestman@users.noreply.github.com>
2024-01-12 08:39:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,6 +17,12 @@ const error_day = document.querySelector(".error-day");
 const error_month = document.querySelector(".error-month");
 const error_year = document.querySelector(".error-year");
 
+let actualDate = new Date();
+let añoActual = actualDate.getFullYear();
+console.log(añoActual);
+
+submit_btn.addEventListener("click", calculateDate);
+
 input_day.addEventListener("input", e => {
     if(+input_day.value > 31){
         error_day.textContent = "Must be a valid date";
@@ -36,3 +42,62 @@ input_day.addEventListener("input", e => {
         error_day.textContent = "";
     }
 });
+
+input_month.addEventListener("input", e => {
+    if(+input_month.value > 12){
+        error_month.textContent = "Must be a valid date";
+        isValid = false;
+        return;
+    }
+    else{
+        isValid = true;
+    }
+    if(+input_month.value === 0){
+        isValid = false;
+        error_month.textContent = "This field is required";
+        isValid = false;
+        return;
+    }
+    else {
+        error_month.textContent = "";
+    }
+});
+
+input_year.addEventListener("input", e => {
+    if(+input_year.value > añoActual){
+        error_year.textContent = "Must be a valid date";
+        isValid = false;
+        return;
+    }
+    else{
+        isValid = true;
+    }
+    if(+input_year.value === 0){
+        isValid = false;
+        error_year.textContent = "This field is required";
+        isValid = false;
+        return;
+    }
+    else {
+        error_year.textContent = "";
+    }
+});
+
+
+function calculateDate(){
+    if(isValid){
+        let birthday = `${input_month.value}/${input_day.value}/${input_year.value}`; 
+        console.log(birthday);
+        let birthdayObj = new Date(birthday);
+        let ageDiffMIll = Date.now() - birthdayObj;
+        let ageDate = new Date(ageDiffMIll);
+        let ageYears = ageDate.getUTCFullYear() - 1970;
+        let ageMonth = ageDate.getUTCMonth();
+        let ageDay = ageDate.getUTCDay();
+        output_day.textContent = ageDay;
+        output_month.textContent = ageMonth;
+        output_year.textContent = ageYears;
+    } else{
+    alert("error")
+ }
+}
